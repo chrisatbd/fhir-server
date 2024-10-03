759f11a45bdd65423826b49f6ffb9a65def11cdf
Chris Hull <chris.hull@bd.com>
2024-10-03 08:50:02 -0700

    Add unit test project

---
+++ src/Microsoft.Health.Fhir.MongoDb.UnitTests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+using System;
+using System.Threading.Tasks;
+using Microsoft.Health.Fhir.Core.Features.Context;
+using Microsoft.Health.Fhir.Tests.Common;
+using Microsoft.Health.Test.Utilities;
+using NSubstitute;
+using Xunit;
+
+namespace Microsoft.Health.Fhir.MongoDb.UnitTests.Extensions
+{
+    [Trait(Traits.OwningTeam, OwningTeam.Fhir)]
+    public class StringExtensionsTests
+    {
+    }
+}
