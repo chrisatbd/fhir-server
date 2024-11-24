ce773fb943d224a2168fd1545a5c9605a702c3e1
Chris Hull <chris.hull@bd.com>
2024-11-23 17:42:20 -0800

    Add empty transaction Handler

---
+++ src/Microsoft.Health.Fhir.MongoDb/Features/Storage/MongoDbTransactionHandler.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Health.Abstractions.Features.Transactions;
+
+namespace Microsoft.Health.Fhir.MongoDb.Features.Storage
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes", Justification = "Pending")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1852:Seal internal types", Justification = "Pending")]
+    internal class MongoDbTransactionHandler : ITransactionHandler
+    {
+        public ITransactionScope BeginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
